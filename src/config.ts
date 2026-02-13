4141c03846f653a51183d609ab7245c3749f0fcf
Hang Li <cecillll.lee@gmail.com>
2026-02-13 15:40:38 +1000

    minor fix of the docker

--- src/config.ts
+++ src/config.ts
@@ -81,10 +81,11 @@ const config: Config = {
 
 // Apply environment variables; coerce numeric values
 const numericKeys = ["PORT", "REDIS_PORT", "TRUST_PROXY", "RATE_LIMIT", "DEFAULT_QUOTA", "MAX_FILE_FOLDER", "MAX_FILE_SIZE", "MAX_REPO_SIZE", "AUTO_DOWNLOAD_REPO_SIZE", "FREE_DOWNLOAD_REPO_SIZE"];
+const configRecord = config as unknown as Record<string, unknown>;
 for (const conf of Object.keys(process.env)) {
-  if ((config as Record<string, unknown>)[conf] !== undefined) {
+  if (configRecord[conf] !== undefined) {
     const val = process.env[conf];
-    (config as Record<string, unknown>)[conf] = numericKeys.includes(conf) && val !== undefined
+    configRecord[conf] = numericKeys.includes(conf) && val !== undefined
       ? Number(val)
       : val;
   }
